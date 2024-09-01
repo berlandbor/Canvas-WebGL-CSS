ebf9684fb5eebb1e654ac674ba56914ef13e4b85
Andrew Berlandbor <141494909+berlandbor@users.noreply.github.com>
2024-09-01 17:03:59 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Highlight the current page in the navigation menu
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('nav ul li a');
+const menuLength = menuItem.length;
+
+for (let i = 0; i < menuLength; i++) {
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = "active";
+    }
+}
